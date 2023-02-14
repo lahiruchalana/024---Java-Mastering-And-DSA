c129443e24991723544b89fe7e45c23947521b65
Lahiru Chalana <lahiruchalana@incubatelabs.com>
2023-02-14 09:58:40 +0530

    Completed ageTree binary tree

---
+++ src/testdatatree/AgeTree.java
@@ -0,0 +1,30 @@
+package testdatatree;
+
+public class AgeTree {
+
+    AgeNode root;
+
+    private AgeNode addRecursive(AgeNode current, int age) {
+        if (current == null) {
+            return new AgeNode(age);
+        }
+
+        if (age < current.age) {
+            current.left = addRecursive(current.left, age);
+        }
+
+        if (age > current.age) {
+            current.right = addRecursive(current.right, age);
+        }
+
+        else {
+            return current;
+        }
+
+        return current;
+    }
+
+    public void addAge(int age) {
+        root = addRecursive(root, age);
+    }
+}
---
+++ src/testdatatree/PostAge.java
@@ -0,0 +1,19 @@
+package testdatatree;
+
+public class PostAge {
+
+    public static void main(String[] args) {
+        AgeTree ages = new AgeTree();
+        ages.addAge(3);
+        ages.addAge(45);
+        ages.addAge(12);
+        ages.addAge(5);
+        ages.addAge(89);
+        ages.addAge(23);
+        ages.addAge(22);
+        ages.addAge(33);
+        ages.addAge(2);
+
+    }
+
+}
