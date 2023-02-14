965b7a3859c91149f985c951d8f52ff2b0ff90d5
Lahiru Chalana <lahiruchalana@incubatelabs.com>
2023-02-14 12:12:41 +0530

    Completed traverse in age tree

--- src/testdatatree/PostAge.java
+++ src/testdatatree/PostAge.java
@@ -13,7 +13,25 @@ public class PostAge {
         ages.addAge(22);
         ages.addAge(33);
         ages.addAge(2);
+        ages.addAge(22); /** if age value already exist in the tree then age value will not add again to tree no duplication **/
+        ages.addAge(89); /** if age value already exist in the tree then age value will not add again to tree no duplication **/
 
+        System.out.println("InOrder Traverse in ages tree!!!");
+        TraverseFunction.inOrderTraverse(ages.root);
+
+        System.out.println();
+        System.out.println("BackOrder Traverse in ages tree!!!");
+        TraverseFunction.backOrderTraverse(ages.root);
+
+        System.out.println();
+        System.out.println("PreOrder Traverse in ages tree!!!");
+        TraverseFunction.preOrderTraverse(ages.root);
+
+        System.out.println();
+        System.out.println("PostOrder Traverse in ages tree!!!");
+        TraverseFunction.postOrderTraverse(ages.root);
+
+        System.out.println();
     }
 
 }
